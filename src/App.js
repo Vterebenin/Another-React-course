fdb8b1b954c748d2b64780368a4fd51d9bfee943
Vterebenin <niceserver11@mail.ru>
2018-08-27 02:04:20 +0300

    prepare for eject in css module

--- src/App.js
+++ src/App.js
@@ -56,7 +56,8 @@ class App extends Component {
 
 	render() {
 		const style = {
-			backgroundColor: 'white',
+			backgroundColor: 'green',
+			color: 'white',
 			font: 'inherit',
 			border: '1px solid blue',
 			padding: '8px',
@@ -78,17 +79,28 @@ class App extends Component {
 						})}
 					</div> 
 				);
+			style.backgroundColor = 'red';
+
 		};
 
+		let classes = []  //"red bold"
+		if (this.state.persons.length <= 2) {
+			classes.push('red'); //classes = ['red']
+		}
+		if (this.state.persons.length <= 1) {
+			classes.push('bold'); //classes = ['red bold']
+		}
+
 		return (
-			<div className="App">
-				<h1>Hi, I'm a React App</h1>
-				<p>This is really working!</p>
-				<button
-					style={style}
-					onClick={this.togglePersonsHandler}>Switch Name</button>
-				{persons}
-			</div>
+				<div className="App">
+					<h1>Hi, I'm a React App</h1>
+					<p className={classes.join(' ')}>This is really working!</p>
+					<button
+						style={style}
+						onClick={this.togglePersonsHandler}>Switch Name</button>
+					{persons}
+				</div>
+
 		);
 	}
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { hot } from 'react-hot-loader'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+export default hot(module)(App)
